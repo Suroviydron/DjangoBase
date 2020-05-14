10c2b50caf9ee2dabec7e17a28d954f30087cb12
SuroviyDron <ablezgov@inbox.ru>
2020-05-15 00:29:42 +0300

    Homework 3

--- geekshoop/mainapp/models.py
+++ geekshoop/mainapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductCategory(models.Model):
+    name = models.CharField(verbose_name="имя", max_length=64, unique=True)
+    description = models.TextField(verbose_name="описание", blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    name = models.CharField(verbose_name="имя", max_length=128)
+    image = models.ImageField(upload_to='product_images', blank=True)
+    short_desc = models.TextField(verbose_name="краткое описание", max_length=60, blank=True)
+    description = models.TextField(verbose_name="описание", blank=True)
+    price = models.DecimalField(verbose_name="цена", max_digits=8, decimal_places=2, default=0)
+    quantity = models.PositiveIntegerField(verbose_name="остаток на складе", default=0)
+
+    def __str__(self):
+        return self.name
